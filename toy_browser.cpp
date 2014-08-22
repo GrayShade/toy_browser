32a7a2b2e1c25ceede2ea631e8ae77b3620c9928
R4DIC4L <teodorescu_oanamaria@yahoo.com>
2014-08-22 21:40:48 +0300

    simplify code

--- toy_browser.cpp
+++ toy_browser.cpp
@@ -46,7 +46,7 @@ public:
 
         for (auto &&attr : attributes_)
             o << ' ' << attr.first << "=\"" << attr.second << '"';
-        
+
         o << '>';
 
         for (auto &&node : children_)
@@ -121,33 +121,24 @@ public:
             {
             case parser_state::before_element:
                 if (*data == '<')
-                {
-                    element_name_.clear();
                     state_ = parser_state::in_element_name;
-                }
                 else
                     throw exception("unexpected input while waiting for element start");
                 break;
             case parser_state::in_element_name:
-                if (*data == '>')
+                if (*data == '>' || *data == ' ')
                 {
                     auto child_element = make_unique<html_element_node>(current_element_, element_name_);
                     auto child_ptr = child_element.get();
                     current_element_->children_.emplace_back(move(child_element));
                     current_element_ = child_ptr;
 
-                    state_ = parser_state::in_element;
-                }
-                else if (*data == ' ')
-                {
-                    auto child_element = make_unique<html_element_node>(current_element_, element_name_);
-                    auto child_ptr = child_element.get();
-                    current_element_->children_.emplace_back(move(child_element));
-                    current_element_ = child_ptr;
+                    element_name_.clear();
 
-                    attribute_name_.clear();
-
-                    state_ = parser_state::before_attribute_name;
+                    if (*data == '>')
+                        state_ = parser_state::in_element;
+                    else
+                        state_ = parser_state::before_attribute_name;
                 }
                 else
                     element_name_ += *data;
@@ -171,8 +162,9 @@ public:
                     state_ = parser_state::before_attribute_value;
                 else if (*data == '>')
                 {
-                    attribute_value_ = attribute_name_;
                     (*current_element_)[attribute_name_] = attribute_name_;
+                    attribute_name_.clear();
+
                     state_ = parser_state::in_element;
                 }
                 else
@@ -190,10 +182,7 @@ public:
                 if (*data == ' ')
                     continue;
                 if (*data == '"' || *data == '\'')
-                {
-                    attribute_value_.clear();
                     state_ = parser_state::attribute_value;
-                }
                 else
                     throw exception("unexpected input while waiting for attribute value");
                 break;
@@ -201,6 +190,9 @@ public:
                 if (*data == '"' || *data == '\'')
                 {
                     (*current_element_)[attribute_name_] = attribute_value_;
+                    attribute_name_.clear();
+                    attribute_value_.clear();
+
                     state_ = parser_state::before_attribute_name;
                 }
                 else
@@ -215,8 +207,6 @@ public:
                         element_text_.clear();
                     }
 
-                    element_name_.clear();
-
                     state_ = parser_state::before_tag_start_or_close;
                 }
                 else
@@ -224,10 +214,7 @@ public:
                 break;
             case parser_state::before_tag_start_or_close:
                 if (*data == '/')
-                {
-                    element_name_.clear();
                     state_ = parser_state::close_element_name;
-                }
                 else
                 {
                     data -= 2;
@@ -254,7 +241,9 @@ public:
                 {
                     if (element_name_ != current_element_->element_name_)
                         throw exception("mismatched close tag");
-                    
+
+                    element_name_.clear();
+
                     current_element_ = current_element_->parent_element();
 
                     state_ = parser_state::in_element;
@@ -273,6 +262,7 @@ int main()
     {
         //string input = "<html foo='bar'></html>";
         string input = "<html x>A<div y>B</div></html>";
+        //string input = "<a>x</a>";
         html_parser parser;
         parser.consume(input.c_str(), input.size());
         parser.root_.print(cerr);
